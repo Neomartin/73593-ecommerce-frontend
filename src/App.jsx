5a1aa17c31349cde2d4b468abeebb0dc3b651fb6
Martin Rodriguez <neomartinr@gmail.com>
2025-06-09 19:16:55 -0300

    Inicio ecommerce React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  console.log('App component rendered');
+
+  return <div>App</div>;
+}
+
+// npm install --save @fortawesome/fontawesome-svg-core
+// npm install --save @fortawesome/free-solid-svg-icons
+// npm install --save @fortawesome/free-regular-svg-icons
+// npm install --save @fortawesome/free-brands-svg-icons
